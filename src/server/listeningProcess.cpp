499c5121da916a594de9e2ee61df191e9a74918d
nmm185 <nmm185@newcastle.ac.uk>
2018-09-21 13:26:52 +0100

    Ignore child process termination at server
    The sigaction() call is used in the listening process in order to start
    ignoring the SIGCHLD signals sent when a client-dedicated process
    terminates.  This is done in order to prevent leaving client-dedicated
    processes in the zombie-state when they are finished.
    
    Ideally, this should be done through a handler that may potentially need
    to do some cleaning up (for example, by informing the distributor
    process to change user state if a user has been logged on through the
    terminated client-dedicated process).  Preliminary implementation of
    such a handler didn't work well and is left for later.  It seemed that
    when this handler was active, new client-dedicated processes were
    spawned when the original one was terminated.  It could have been due to
    incorrect implementation of the wait() function, but this hasn't been
    thoroughly tested.

--- src/server/listeningProcess.cpp
+++ src/server/listeningProcess.cpp
@@ -1,7 +1,27 @@
 #include "listeningProcess.h"
+#include <signal.h>
+#include <wait.h>
 #include <fstream>
 
+void signalHandler(int signal, siginfo_t* signalInfo, void* context) {
+  int status;
+  wait(&status);
+}
+
 int ListeningProcess::run() {
+  /*
+   * Set up a handler for SIGCHLD signals that are sent to the process when
+   * client-dedicated processes finish.  This should help with removing zombie
+   * processes.
+   */
+  struct sigaction signalAction;
+  // pointer to the handler function
+  /* DBG: Ignore SIGCHLD in the first instance; intercept with a proper handler later! */
+  //signalAction.sa_sigaction = &signalHandler;
+  //signalAction.sa_flags = SA_SIGINFO;
+  signalAction.sa_handler = SIG_DFL;
+  signalAction.sa_flags = SA_NOCLDWAIT;
+  sigaction(SIGCHLD, &signalAction, 0);
   /*
    * Listen to incoming connection requests.  When one is received, accept it and
    * spawn a new process to deal with it.
